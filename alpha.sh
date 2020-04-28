cbe14be5531ffc187b78294000887ff554be31ef
anilsalunke2019 <52329545+anilsalunke2019@users.noreply.github.com>
2020-04-28 20:51:49 +0530

    dsf
    sdf

--- alpha.sh
+++ alpha.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /uploads/videos/Qatar/; for i in `cat /root/file1`;do /usr/local/bin/youtube-dl --no-playlist  -f 22 $i; done
+cd /uploads/videos/Qatar/; for i in `cat /root/file1`;do /usr/local/bin/youtube-dl --no-playlist  -f best $i; done
